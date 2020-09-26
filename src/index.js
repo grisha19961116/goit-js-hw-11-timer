56c11a1d1a8c00dffefe3149701689e20b404a23
unknown <grisha1996111616@gmail.com>
2020-09-26 20:23:36 +0100

    i've finished exercise 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import './styles.css';
-import './java-script/switch.js'
-import objectData from './menu.json'
-import menuTemplate from './templates/galery.hbs';
-const markup = menuTemplate(objectData);
-const redyRenderingUl = document.querySelector('.js-menu');
-redyRenderingUl.insertAdjacentHTML('afterbegin', markup);
+import  './java-script/timer.js';
 
 
 
---
+++ src/java-script/timer.js
@@ -0,0 +1,56 @@
+export
+ class Timer {
+  constructor(dateNow, targetDate,day = 0 ,hour = 0,mins = 0,secs = 0) {
+    this.dateNow = dateNow;
+    this.targetDate = targetDate;
+    this.day = day;
+    this.hour = hour;
+    this.mins = mins;
+    this.secs = secs;
+  }
+  showTimes(time){
+    return time;
+  }
+  getDays(time){
+    return Math.floor(time / (1000 * 60 * 60 * 24));
+  }
+  getHours(time){
+    return this.hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  };
+  getMins(time){
+    return Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  };
+  getSecs(time){
+    return  Math.floor((time % (1000 * 60)) / 1000);
+  }
+}
+
+
+
+const showDay = document.querySelector('span[data-value="days"]');
+
+
+const showHour = document.querySelector('span[data-value="hours"]');
+
+
+const showMins = document.querySelector('span[data-value="mins"]');
+
+
+const showSecs = document.querySelector('span[data-value="secs"]');
+
+
+const start = () => {
+  setInterval(() => {
+    const dateNow = Date.now();
+    const targetDate = new Date('2021, 0, 30').getTime();
+    console.log(dateNow);
+    console.log(targetDate);
+    const time = (targetDate-dateNow);
+    const newTimer = new Timer(dateNow,targetDate); 
+    showDay.textContent = (newTimer.getDays(time));
+    showHour.textContent = (newTimer.getHours(time));
+    showMins.textContent = (newTimer.getMins(time));
+    showSecs.textContent = (newTimer.getSecs(time));
+  }, 1000)
+};
+start()
