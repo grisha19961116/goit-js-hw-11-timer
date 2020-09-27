b3df0170c35a28db51b661d495e769c865728269
unknown <grisha1996111616@gmail.com>
2020-09-27 16:07:52 +0100

    fix something

--- src/java-script/timer.js
+++ src/java-script/timer.js
@@ -1,54 +1,41 @@
 export
  class Timer {
-  constructor(dateNow, targetDate,day = 0 ,hour = 0,mins = 0,secs = 0) {
+  constructor({dateNow, targetDate,day = 0 ,hour = 0,mins = 0,secs = 0}) {
     this.dateNow = dateNow;
     this.targetDate = targetDate;
+    this.time = this.targetDate-this.dateNow;
     this.day = day;
     this.hour = hour;
     this.mins = mins;
     this.secs = secs;
   }
-  showTimes(time){
-    return time;
+  showTimes(){
+    return this.time;
   }
-  getDays(time){
-    return Math.floor(time / (1000 * 60 * 60 * 24));
+  getDays(){
+    return Math.floor(this.time / (1000 * 60 * 60 * 24));
   }
-  getHours(time){
-    return this.hour = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  getHours(){
+    return this.hour = Math.floor((this.time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   };
-  getMins(time){
-    return Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  getMins(){
+    return Math.floor((this.time % (1000 * 60 * 60)) / (1000 * 60));
   };
-  getSecs(time){
-    return  Math.floor((time % (1000 * 60)) / 1000);
+  getSecs(){
+    return  Math.floor((this.time % (1000 * 60)) / 1000);
   }
 }
-
-
-
 const showDay = document.querySelector('span[data-value="days"]');
-
-
 const showHour = document.querySelector('span[data-value="hours"]');
-
-
 const showMins = document.querySelector('span[data-value="mins"]');
-
-
 const showSecs = document.querySelector('span[data-value="secs"]');
-
-const targetDate = new Date('2021, 0, 30').getTime();
-const newTimer = new Timer(); 
-const start = () => {
+const startTimer = () => {
   setInterval(() => {
-    const dateNow = Date.now();
-    const time = targetDate-dateNow;
-    showDay.textContent = `${newTimer.getDays(time)}` ;
-    showHour.textContent = `${newTimer.getHours(time)}`;
-    showMins.textContent = `${newTimer.getMins(time)}`;
-    showSecs.textContent = `${newTimer.getSecs(time)}`;
-    console.log(`${newTimer.getSecs(time)}`);
+    const newTimer = new Timer({dateNow : Date.now(),targetDate : new Date(2021, 0, 30).getTime()}); 
+    showDay.textContent = `${newTimer.getDays()}`;
+    showHour.textContent = `${newTimer.getHours()}`;
+    showMins.textContent = `${newTimer.getMins()}`;
+    showSecs.textContent = `${newTimer.getSecs()}`;
   }, 1000)
 };
-start()
+startTimer()
